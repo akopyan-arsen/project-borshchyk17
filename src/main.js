3acbcac377ee01e38ea29a1d343af01422cc1765
akopyan-arsen <149099397+akopyan-arsen@users.noreply.github.com>
2024-01-15 17:52:43 +0200

    added js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-function closeModal() {
-  document.getElementById('myModal').style.display = 'none';
+function openModal() {
+  document.getElementById('myModal').classList.add('is-open');
 }
+
+function closeModal() {
+  document.getElementById('myModal').classList.remove('is-open');
+}
+
+var menuLinks = document.querySelectorAll('.menu-list');
+
+function closeMobileMenu() {
+  document.getElementById('myModal').classList.remove('is-open');
+}
+
+menuLinks.forEach(function(link) {
+  link.addEventListener('click', closeMobileMenu);
+});
